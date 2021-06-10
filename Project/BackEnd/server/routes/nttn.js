dd497ee42053d1dd1702a9aefa6ad8b48b6b73a8
zahinahmed12 <64348148+zahinahmed12@users.noreply.github.com>
2021-06-10 23:14:16 +0600

    nttn route added

---
+++ Project/BackEnd/server/routes/nttn.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const nttnReportController = require("../controller/nttnReportController");
+const nttnFeedbackController = require("../controller/nttnFeedbackController");
+const nttnRequestController = require("../controller/nttnRequestController");
+
+const nttnAfterPaymentController = require("../controller/nttnAfterPaymentController");
+
+// manage routes
+
+// fetch the reports of all the isps
+router.get("/reports", nttnReportController.handleReportFetching);
+
+// fetch the reports sorted by
+router.post("/reports/sortBy", nttnReportController.handleReportFetchingSorted);
+
+// view a particular report
+router.post("/reports/view", nttnReportController.handleOneReport);
+
+// set report status to solved and report solving time to current time
+router.post("/reports/solve", nttnReportController.handleSolvedReport);
+
+// view the feedbacks of users
+router.get("/feedbacks", nttnFeedbackController.handleFeedbacks);
+
+// view a particular feedback
+router.post("/feedbacks/view", nttnFeedbackController.handleOneFeedback);
+
+// sort the feedbacks based on district, division, subdistrict, union, area, isp
+router.post("/feedbacks/sortBy", nttnFeedbackController.handleFeedbackSorted);
+
+// pending requests
+router.get("/pending", nttnRequestController.handlePending);
+
+//post pending requests
+router.post("/pending", nttnRequestController.insertPending);
+
+// pending/notify
+// router.post('/pending/notify', nttnRequestController.handlePendningNotification);
+
+// renewal request
+router.get("/renewal", nttnRequestController.handleRenewal);
+
+// renewal/notify
+// router.post('/renewal/notify', nttnrequestController.handleRenewalNotification);
+
+// fetch all the new payments
+router.get("/payments", nttnAfterPaymentController.findNewPayment);
+
+// set payment status to solved and solving time to current time
+router.post("/payments/resolve", nttnAfterPaymentController.handlePaymentDone);
+
+module.exports = router;
